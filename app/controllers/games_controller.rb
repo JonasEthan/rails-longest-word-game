c4cac395f78b1be6e310118320310c2c9957d843
Jonas Ethan Springer <springerjonasethan@gmail.com>
2022-05-16 17:15:06 +0200

    Submitting my work to Kitt not done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+require 'json'
 class GamesController < ApplicationController
 
   def index
@@ -6,5 +8,30 @@ class GamesController < ApplicationController
 
     10.times { @letters << alphabet[rand(0...alphabet.size)] }
     # raise
+    @letters
+  end
+
+  def score
+    @letters = params[:letters].split
+    @word = params[:word].downcase
+    @included = included?(@letters, @word)
+    @is_word = is_word?(@word)
+  end
+
+  private
+
+  def included?(letters, word)
+    word.chars.all? do |char|
+      if letters.include?(char)
+        index = letters.index(char)
+        letters.delete_at(index)
+      end
+    end
+  end
+
+  def is_word?(word)
+    uri = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    check = JSON.parse(uri.read)
+    check['found']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'games#index'
+  get '/score', to: 'games#score'
 end
